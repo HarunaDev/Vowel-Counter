fce8610dc9aa3a1789021f1d066099c577a21c52
Haruna Alvin <harunaalvin69@gmail.com>
2022-05-28 02:30:12 +0100

    added vowel counting function

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 // Grab elements 
 const inputEl = document.getElementById("input-el")
 const submitBtn = document.getElementById("btn")
+
+// submit event
+submitBtn.addEventListener("click", () => {
+    const countVowels = inputEl.value = Array.from(inputEl.value).filter(letter => `aeiou`.includes(letter)).length
+  
+    inputEl.value=""
+  
+    return alert(`${countVowels}`)
+  
+    
+  })
